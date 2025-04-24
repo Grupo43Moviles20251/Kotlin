6e00cf5ac6aa1ae4504347bf8187769dbd94ffa8
Leonardo3310 <jl.rangel@uniandes.edu.co>
2025-04-24 10:26:18 -0500

    profile screen con local storage + caching strategies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -73,6 +73,7 @@ dependencies {
     implementation(libs.firebase.firestore.ktx)
     implementation(libs.play.services.analytics.impl)
     implementation(libs.play.services.location)
+    implementation(libs.firebase.storage.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
